a9527c9ab43b4c668c961243454a2c00bb2f11c5
Gemy2050 <88037743+Gemy2050@users.noreply.github.com>
2022-10-31 20:59:18 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+let parent = document.querySelector(".container .parent");
+let iFrame = document.querySelector(".container iframe");
+let name = document.querySelector(".container .name");
+
+
+iFrame.onload = function() {
+  console.log("Loaded");
+  
+}
+
+
+fetch("https://quran-endpoint.vercel.app/quran")
+.then((res)=> res.json())
+.then((data)=> {
+  name.textContent = data.data[0].asma.ar.long;
+  for(let i=0; i<data.data.length; i++) {
+
+    parent.innerHTML += `<div class='box' data-num='${`${i+1}`.zFill(3)}'>${data.data[i].asma.ar.long}<div/>`;
+  }
+
+  handleClick();
+
+});
+
+
+function handleClick(url) {
+  let boxs = document.querySelectorAll(".parent .box");
+
+  boxs.forEach((box)=> {
+    box.addEventListener(('click'), (e)=> {
+      iFrame.src = `https://download.quranicaudio.com/quran/ahmed_ibn_3ali_al-3ajamy/${e.target.dataset.num}.mp3`;
+      name.textContent = e.target.textContent;
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      })
+    })
+  })
+
+}
+
+
+String.prototype.zFill = function (width) {
+    let result = this;
+    while(width > result.length) {
+        result = `0${result}`
+    }
+    return result.toString();
+}
